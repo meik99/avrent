ddd329208c0ccb51354a2a3a87f64de24d43c0b0
Michael Rynkiewicz <michaelrynkiewicz3@gmail.com>
2017-04-08 12:39:49 +0200

    Iterate over equipment and displaying it on index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var database = require("../src/database")();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'AVRent' });
+  res.render('index', { title: 'AVRent', equipment:  database.equipment().findAll()});
 });
 
 module.exports = router;
