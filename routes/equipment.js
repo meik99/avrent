a645e6fc9662a64deb70507d12975bd6654cd883
Michael Rynkiewicz <michaelrynkiewicz3@gmail.com>
2017-04-13 22:37:35 +0200

    Added link to /equipment

--- routes/equipment.js
+++ routes/equipment.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var database = require("../database");
 
+var equipmentDao = require("../dao/equipment");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send(database.equipment().findAll());
+  
 });
 
 module.exports = router;
