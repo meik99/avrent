0777d51dd58fe734b14dafec90b68594fe4c331b
Michael Rynkiewicz <michaelrynkiewicz3@gmail.com>
2017-04-14 22:30:52 +0200

    Implemented layout to add and delete equipment

--- routes/equipment.js
+++ routes/equipment.js
@@ -5,7 +5,14 @@ var equipmentDao = require("../dao/equipment");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
+    equipmentDao.findEquipmentWithRentals(
+        req.models.equipment,
+        req.models.rental,
+        function (result) {
+            res.render("equipment/index", {
+                equipment: result
+            });
+        });
 });
 
 module.exports = router;
