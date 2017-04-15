dab1da6f9fd77a41771b388c79164021c7e991a2
Michael Rynkiewicz <michaelrynkiewicz3@gmail.com>
2017-04-15 14:49:04 +0200

    Implemented managing equipment

--- routes/equipment.js
+++ routes/equipment.js
@@ -4,11 +4,16 @@ var router = express.Router();
 var equipmentDao = require("../dao/equipment");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     equipmentDao.findEquipmentWithRentals(
         req.models.equipment,
         req.models.rental,
         function (result) {
+
+            result = result.sort(function(a, b){
+                return a.id.localeCompare(b.id);
+            });
+
             res.render("equipment/index", {
                 equipment: result
             });
@@ -16,15 +21,99 @@ router.get('/', function(req, res, next) {
 });
 
 router.post("/", function (req, res) {
-   var id = req.body.id;
+    var id = req.body.id;
 
-   if(id){
-       req.models.equipment.find({id: id}, {}, function (err, results) {
-          if(results.length > 0){
-              res.send({error: "Gerät mit dieser Id bereits vorhanden!"})
-          }
+    console.log(req.body);
+    if (id) {
+        req.models.equipment.find({name: id}, {}, function (err, results) {
+            if(err) console.log(err);
+
+            if (results.length > 0) {
+                res.send({error: "Gerät mit dieser Id bereits vorhanden!"})
+            }
+            else {
+                req.models.equipment.create({name: id}, function (err, result) {
+                    if(err) console.log(err);
+
+                    res.send({});
+                });
+            }
+        });
+    } else {
+        res.send({error: "Ungültige Id"});
+    }
+});
+
+router.put("/", function (req, res) {
+    var name = req.body.id;
+    var oldName = req.body.oldId;
+
+    if (name && oldName) {
+        req.models.equipment.find({name: oldName}, function (err, result) {
+            if (err) console.log(err);
+
+            if (result.length > 1) {
+                res.send({error: "Schwerwiegender Fehler: Mehrere Geräte mit gleicher Id gefunden."})
+            }
+            else if (result.length <= 0) {
+                res.send({error: "Gewähltes Gerät nicht gefunden"})
+            } else {
+                req.models.equipment.find({name: name}, function (err, results) {
+                    if(err) console.log(err);
+
+                    if (results.length > 0) {
+                        res.send({error: "Gerät mit dieser Id bereits vorhanden!"})
+                    } else {
+                        result[0].name = name;
+                        result[0].save(function (err) {
+                            if(err) console.log(err);
+
+                            res.send({});
+                        });
+                    }
+                });
+            }
+        });
+    } else {
+        res.send({error: "Ungültige Ids"});
+    }
+});
+
+router.delete("/", function(req, res){
+   var name = req.body.id;
+
+
+   if(name){
+       req.models.equipment.find({name: name}, function (err, results) {
+           if(err) console.log(err);
+
+           if (results.length > 1) {
+               res.send({error: "Schwerwiegender Fehler: Mehrere Geräte mit gleicher Id gefunden."})
+           }
+           else if(results.length <= 0){
+               res.send({})
+           }
+           else{
+               var nameToDelete = results[0].name;
+
+               req.models.rental.find({equipmentName: nameToDelete}, function (err, rentals) {
+                   if(err) console.log(err);
+
+                   if(rentals.length > 0){
+                       res.send({error: "Gerät wird derzeit verliehen und kann nicht gelöscht werden"})
+                   }else{
+                       results[0].remove(function (err) {
+                           if(err) console.log(err);
+                           res.send({});
+                       });
+                   }
+               });
+           }
        });
+   }else{
+       res.send({error: "Ungültige Id"});
    }
+
 });
 
 module.exports = router;
